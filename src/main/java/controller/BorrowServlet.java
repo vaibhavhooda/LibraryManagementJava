edac09ad0ab6fafa3a9ad5601e95debb47fc7f53
DhimantGC0796370 <C0796370@mylambton.ca>
2022-03-02 01:39:36 -0500

    borrow code

---
+++ src/main/java/controller/BorrowServlet.java
@@ -0,0 +1,56 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.BorrowDao;
+import model.Borrow;
+
+/**
+ * Servlet implementation class BorrowServlet
+ */
+@WebServlet("/BorrowServlet")
+public class BorrowServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private BorrowDao bdao;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BorrowServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		ArrayList<Borrow> borrow = new ArrayList<Borrow>();
+		try {
+			borrow = bdao.getBorrowDetails();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		request.setAttribute("borrow", borrow);
+		response.setContentType("text/html;charset=UTF-8");
+		request.getRequestDispatcher("Borrow.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
